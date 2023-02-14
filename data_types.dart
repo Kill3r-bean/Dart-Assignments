9e651d073588160b6f477723e2e4f252b030cca8
Jonathan Banda <jaybanda21091@gmail.com>
2023-02-14 04:44:21 +0200

    created data types assignment

---
+++ data_types.dart
@@ -0,0 +1,17 @@
+void main(){
+   //example of an integer data type, only store wholenumbers.
+  int myNumber = 20;
+  print(myNumber);
+    //example of the double data type, is able to store decimals 
+  double myFloat = 16.5;
+  print(myFloat);
+    //example of the string data type that stores a sequence of characters
+  String myWord = "This is an example of a string";
+  print(myWord);
+    //example of a list data type which can store a range of items
+  List fruits = ['apple','banana', 'orange',];
+  print(fruits[1]);
+    //example of the map data type which store key values
+  Map studentDetails = {'Name':'Jay Banda','Email':'jaybanda21091@gmail.com','Course':'Dart with Flutter'};
+  print(studentDetails);
+}
